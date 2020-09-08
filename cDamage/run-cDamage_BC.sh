55bed6c8ddef7ebb56e946c14f344fdc6a7b8b56
Murray Journeay <53489116+mjourneay@users.noreply.github.com>
2020-09-08 09:06:01 -0700

    Updates to job.ini files (#5)
    * test run - CanadaSHM6_BC
    
    testing CanadaSHM6 hazard models for Western Canada
    
    * Update cHazardSHM6_BC.ini
    
    * updates and repo cleanup
    
    * update cHazard_BC.ini
    
    * cleanup
    
    * Update oq-parameters.numbers
    
    * model scripts and input parameters used to generate CanadaSRM2
    
    lookup tables and sample job.ini /batch.sh files used to generate CanadaSRM2 model
    
    * revisions to directory structures and corresponding job.ini files
    
    * added git-lfs tracking for all output folders
    
    * fixed errors with cHazard file
    
    * updates model parameters to fix job.ini errors
    
    * revisions to job.ini files
    
    * Update run-cDamage_BC.sh
    
    * Updated ebRisk job parameters
    
    * Update model-parameters_.numbers

--- cDamage/run-cDamage_BC.sh
+++ cDamage/run-cDamage_BC.sh
@@ -2,20 +2,79 @@
 # =================================================================
 # Classical damage calculation script for British Columbia (CanadaSRM2)
 # =================================================================
-#[region]
+#[BC5910]
 # =================================================================
 # Baseline Conditions
-oq engine --run input/cDamage_[region]_b0.ini --hc [calc-id] &> ../output/BC/cD_[region]_b0.log;
-oq export fullreport -1 -e rst -d ../output/BC/cD_[region]_report.rst
-oq export realizations -1 -e csv -d ../output/BC/cD_[region]_rlz.csv
-oq export sourcegroups -1 -e csv -d ../output/BC/cD_[region]_sources.csv
-oq export dmg_by_asset -e csv -d ../output/BC/cD_[region]_dmg_by_asset_b0.csv;
+oq engine --run input/cDamage_BC5910_b0.ini --hc 786 &> output/BC/cD_BC5910_b0.log;
+oq export fullreport -1 -e rst -d output/BC/
+mv output/BC/report*.rst output/BC/output/BC/cD_BC5910_report.rst;
+oq export realizations -1 -e csv -d output/BC/
+mv output/BC/realizations*.csv output/BC/cD_BC5910_rlz.csv;
+oq export damages-stats -1 -e csv -d output/BC
+mv output/BC/damages-mean_*.csv output/BC/cD_BC5910_dmg-mean_b0.csv;
 # Level 2 Retrofit
-oq engine --run input/cDamage_[region]_r2.ini --hc [calc-id] &> ../output/BC/cD_[region]_r2.log;
-oq export fullreport -1 -e rst -d ../output/BC/cD_[region]_report.rst
-oq export realizations -1 -e csv -d ../output/BC/cD_[region]_rlz.csv
-oq export sourcegroups -1 -e csv -d ../output/BC/cD_[region]_sources.csv
-oq export dmg_by_asset -e csv -d ../output/BC/cD_[region]_dmg_by_asset_r2.csv;
+oq engine --run input/cDamage_BC5910_r2.ini --hc 786 &> output/BC/cD_BC5910_r2.log;
+oq export damages-stats -1 -e csv -d output/BC
+mv output/BC/damages-mean_*.csv output/BC/cD_BC5910_dmg-mean_r2.csv;
 # =================================================================
-# replicate for all regions 
+#BC5920A
 # =================================================================
+# Baseline Conditions
+oq engine --run input/cDamage_BC5920A_b0.ini --hc 786 &> output/BC/cD_BC5920A_b0.log;
+oq export fullreport -1 -e rst -d output/BC/
+mv output/BC/report*.rst output/BC/cD_BC5920A_report.rst;
+oq export realizations -1 -e csv -d output/BC/
+mv output/BC/realizations*.csv output/BC/cD_BC5920A_rlz.csv;
+oq export damages-stats -1 -e csv -d output/BC/
+mv output/BC/damages-mean_*.csv output/BC/cD_BC5920A_dmg-mean_b0.csv;
+ Level 2 Retrofit
+oq engine --run input/cDamage_BC5920A_r2.ini --hc 786 &> output/BC/cD_BC5920A_r2.log;
+oq export damages-stats -1 -e csv -d output/BC/
+mv output/BC/damages-mean_*.csv output/BC/cD_BC5920A_dmg-mean_r2.csv;
+ # =================================================================
+#[BC5920B]
+# =================================================================
+# Baseline Conditions
+oq engine --run input/cDamage_BC5920B_b0.ini --hc 786 &> output/BC/cD_BC5920B_b0.log;
+oq export fullreport -1 -e rst -d output/BC/
+mv output/BC/report*.rst output/BC/cD_BC5920B_report.rst;
+oq export realizations -1 -e csv -d output/BC/
+mv output/BC/realizations*.csv output/BC/cD_BC5920B_rlz.csv;
+oq export damages-stats -1 -e csv -d output/BC
+mv output/BC/damages-mean_*.csv output/BC/cD_BC5920B_dmg-mean_b0.csv;
+# Level 2 Retrofit
+oq engine --run input/cDamage_BC5920B_r2.ini --hc 786 &> output/BC/cD_BC5920B_r2.log;
+oq export damages-stats -1 -e csv -d output/BC/
+mv output/BC/damages-mean_*.csv output/BC/cD_BC5920B_dmg-mean_r2.csv;
+ # =================================================================
+#[BC5930]
+# =================================================================
+# Baseline Conditions
+oq engine --run input/cDamage_BC5930_b0.ini --hc 786 &> output/BC/cD_BC5930_b0.log;
+oq export fullreport -1 -e rst -d output/BC/
+mv output/BC/report*.rst output/BC/cD_BC5930_report.rst;
+oq export realizations -1 -e csv -d output/BC/
+mv output/BC/realizations*.csv output/BC/cD_BC5930_rlz.csv;
+oq export damages-stats -1 -e csv -d output/BC/
+mv output/BC/damages-mean_*.csv output/BC/cD_BC5930_dmg-mean_b0.csv;
+# Level 2 Retrofit
+oq engine --run input/cDamage_BC5930_r2.ini --hc 786 &> output/BC/cD_BC5930_r2.log;
+oq export damages-stats -1 -e csv -d output/BC/
+mv output/BC/damages-mean_*.csv output/BC/cD_BC5930_dmg-mean_r2.csv;
+ # =================================================================
+#[BC5940-80]
+# =================================================================
+# Baseline Conditions
+oq engine --run input/cDamage_BC5940-80_b0.ini --hc 786 &> output/BC/cD_BC5940-80_b0.log;
+oq export fullreport -1 -e rst -d output/BC/
+mv output/BC/report*.rst output/BC/cD_BC5940-80_report.rst;
+oq export realizations -1 -e csv -d output/BC/
+mv output/BC/realizations*.csv output/BC/cD_BC5940-80_rlz.csv;
+oq export damages-stats -1 -e csv -d output/BC/
+mv output/BC/damages-mean_*.csv output/BC/cD_BC5940-80_dmg-mean_b0.csv;
+# Level 2 Retrofit
+oq engine --run input/cDamage_BC5940-80_r2.ini --hc 786 &> output/BC/cD_BC5940-80_r2.log;
+oq export damages-stats -1 -e csv -d output/BC/
+mv output/BC/damages-mean_*.csv output/BC/cD_BC5940-80_dmg-mean_r2.csv;
+# =================================================================
+#end
