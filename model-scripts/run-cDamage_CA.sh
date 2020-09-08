a5d0ca751125652ae8d027caa00a1d410ed6e16a
Murray Journeay <murray.journeay@canada.ca>
2020-09-07 20:45:45 -0700

    revisions to job.ini files

--- model-scripts/run-cDamage_CA.sh
+++ model-scripts/run-cDamage_CA.sh
@@ -7,15 +7,15 @@
 # Baseline Conditions
 oq engine --run input/cDamage_[region]_b0.ini --hc [calc-id] &> ../output/[province]/cD_[region]_b0.log;
 oq export fullreport -1 -e rst -d ../output/[province]/cD_[region]_report.rst
-oq export realizations -1 -e csv -d ../output/[province]/cD_[region]_rlz.csv
-oq export sourcegroups -1 -e csv -d ../output/[province]/cD_[region]_sources.csv
-oq export dmg_by_asset -e csv -d ../output/[province]/cD_[region]_dmg_by_asset_b0.csv;
+mv ../output/[province]/report*.rst ../output/[province]/cD_[region]_report.rst
+oq export realizations -1 -e csv -d ../output/[province]/
+mv ../output/[province]/realizations*.csv ../output/[province]/cD_[region]_rlz.csv
+oq export damages-stats -e csv -d ../output/[province]
+ mv ../output/[province]/damages-mean_*.csv ../output/[province]/cD_[region]_dmg-mean_b0.csv;
 # Level 2 Retrofit
 oq engine --run input/cDamage_[region]_r2.ini --hc [calc-id] &> ../output/[province]/cD_[region]_r2.log;
-oq export fullreport -1 -e rst -d ../output/[province]/cD_[region]_report.rst
-oq export realizations -1 -e csv -d ../output/[province]/cD_[region]_rlz.csv
-oq export sourcegroups -1 -e csv -d ../output/[province]/cD_[region]_sources.csv
-oq export dmg_by_asset -e csv -d ../output/[province]/cD_[region]_dmg_by_asset_r2.csv;
+oq export damages-stats -e csv -d ../output/[province]
+ mv ../output/[province]/damages-mean_*.csv ../output/[province]/cD_[region]_dmg-mean_r2.csv;
 # =================================================================
 # replicate for all regions 
 # =================================================================
--- model-scripts/run-ebRisk_CA.sh
+++ model-scripts/run-ebRisk_CA.sh
@@ -5,21 +5,33 @@
 #[region]
 # =================================================================
 # Baseline Conditions
-oq engine --run ebRisk_[region]_b0.ini &> ../output/[province]/ebR_[region]_b0.log;
-oq export fullreport -1 -e rst -d ../output/[province]/ebR_[region]_report.rst
-oq export realizations -1 -e csv -d ../output/[province]/ebR_[region]_rlz.csv
-oq export sourcegroups -1 -e csv -d ../output/[province]/ebR_[region]_sources.csv
-oq export avg_losses-stats -e csv -d ../output/[province]/ebR_[region]_avg_losses-stats_b0.csv;
-oq export agg_curves-stats -e csv -d ../output/[province]/ebR_[region]_agg_curves-stats_b0.csv;
-oq export agg_losses-stats -e csv -d ../output/[province]/ebR_[region]_agg_losses-stats_b0.csv;
+oq engine --run ebRisk_[region]_b0.ini &> output/[province]/ebR_[region]_b0.log;
+oq export fullreport -1 -e rst -d output/[province]/
+mv output/[province]/report*.rst output/[province]/ebR_[region]_report.rst
+oq export realizations -1 -e csv -d output/[province]/
+mv output/[province]/realizations*.csv output/[province]/ebR_[region]_rlz.csv
+oq export sourcegroups -1 -e csv -d output/[province]/
+mv output/[province]/sourcegroups*.csv output/[province]/ebR_[region]_sources.csv
+oq export avg_losses-stats -e csv -d output/[province]/
+mv output/[province]/sourcegroups*.csv output/[province]/ebR_[region]_avg_losses-stats_b0.csv;
+oq export agg_curves-stats -e csv -d output/[province]/
+mv output/[province]/agg_curves-stats*.csv output/[province]/ebR_[region]_agg_curves-stats_b0.csv;
+oq export agg_losses-stats -e csv -d output/[province]/
+mv output/[province]/agg_losses-stats*.csv output/[province]/ebR_[region]_agg_losses-stats_b0.csv;
 # Level 2 Retrofit
-oq engine --run ebRisk_[region]_r2.ini &> ../output/[province]/ebR_[region]_r2.log;
-oq export fullreport -1 -e rst -d ../output/[province]/ebR_[region]_report.rst
-oq export realizations -1 -e csv -d ../output/[province]/ebR_[region]_rlz.csv
-oq export sourcegroups -1 -e csv -d ../output/[province]/ebR_[region]_sources.csv
-oq export avg_losses-stats -e csv -d ../output/[province]/ebR_[region]_avg_losses-stats_r2.csv;
-oq export agg_curves-stats -e csv -d ../output/[province]/ebR_[region]_agg_curves-stats_r2.csv;
-oq export agg_losses-stats -e csv -d ../output/[province]/ebR_[region]_agg_losses-stats_r2.csv;
+oq engine --run ebRisk_[region]_r2.ini &> output/[province]/ebR_[region]_r2.log;
+oq export fullreport -1 -e rst -d output/[province]/
+mv output/[province]/report*.rst output/[province]/ebR_[region]_report.rst
+oq export realizations -1 -e csv -d output/[province]/
+mv output/[province]/realizations*.csv output/[province]/ebR_[region]_rlz.csv
+oq export sourcegroups -1 -e csv -d output/[province]/
+mv output/[province]/sourcegroups*.csv output/[province]/ebR_[region]_sources.csv
+oq export avg_losses-stats -e csv -d output/[province]/
+mv output/[province]/sourcegroups*.csv output/[province]/ebR_[region]_avg_losses-stats_r2.csv;
+oq export agg_curves-stats -e csv -d output/[province]/
+mv output/[province]/agg_curves-stats*.csv output/[province]/ebR_[region]_agg_curves-stats_r2.csv;
+oq export agg_losses-stats -e csv -d output/[province]/
+mv output/[province]/agg_losses-stats*.csv output/[province]/ebR_[region]_agg_losses-stats_r2.csv;
 # =================================================================
 # replicate for all regions 
 # =================================================================
