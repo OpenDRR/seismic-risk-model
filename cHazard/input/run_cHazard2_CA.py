eb2ba09a8e355d8e68db5c275727185b2ef9e577
Murray Journeay <murray.journeay@canada.ca>
2020-09-17 08:57:58 -0700

    Minor updates
    Trouble-shooting issues with bash scripts not recognizing files in directory structure on Azure

---
+++ cHazard/input/run_cHazard2_CA.py
@@ -0,0 +1,5 @@
+$ python3
+Python 3.6.7 (default, Oct 22 2018, 11:32:17)
+[GCC 8.2.0] on linux
+Type "help", "copyright", "credits" or "license" for more information.
+>>>
---
+++ cHazard/input/run_cHazard_CA.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#
+oq engine --run cHazard_NS_5k.ini
+oq engine --run cHazard_NT_5k.ini
+oq engine --run cHazard_NU_5k.ini
+oq engine --run cHazard_ON_5k.ini
+oq engine --run cHazard_PE_5k.ini
+oq engine --run cHazard_QC_5k.ini
+oq engine --run cHazard_SK_5k.ini
+oq engine --run cHazard_YT_5k.ini
