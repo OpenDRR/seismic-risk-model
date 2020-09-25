886d5d8d8f99e7c8443eaeb1e2906cef3b72e029
Drew Rotheram <drew.rotheram@gmail.coom>
2020-09-25 14:37:05 -0700

    Put job files into neat directories

--- model-scripts/generateSRMJobFiles.py
+++ model-scripts/generateSRMJobFiles.py
@@ -37,16 +37,19 @@ def main():
 def write_c_damage_config(regionPair):
     province = regionPair[1]
     region = regionPair[0]
+    if not os.path.exists('cDamage_SRMJobFiles_output'):
+        os.makedirs('cDamage_SRMJobFiles_output')
+
     configTemplate = open('cDamage_b0_template.ini', 'r').read()
     configContents = configTemplate.format(**{'region':region,'province':province})
-    configFile = open('cDamage_b0_{region}.ini'.format(**{'region':region}), 'w')
+    configFile = open('cDamage_SRMJobFiles_output/cDamage_b0_{region}.ini'.format(**{'region':region}), 'w')
     configFile.write(configContents)
     configFile.close()
     # configTemplate.close()
 
     configTemplate = open('cDamage_r2_template.ini', 'r').read()
     configContents = configTemplate.format(**{'region':region,'province':province})
-    configFile = open('cDamage_r2_{region}.ini'.format(**{'region':region}), 'w')
+    configFile = open('cDamage_SRMJobFiles_output/cDamage_r2_{region}.ini'.format(**{'region':region}), 'w')
     configFile.write(configContents)
     configFile.close()
     # configTemplate.close()
@@ -55,16 +58,19 @@ def write_c_damage_config(regionPair):
 def write_e_damage_config(regionPair):
     province = regionPair[1]
     region = regionPair[0]
+    if not os.path.exists('eDamage_SRMJobFiles_output'):
+        os.makedirs('eDamage_SRMJobFiles_output')
+
     configTemplate = open('eDamage_b0_template.ini', 'r').read()
     configContents = configTemplate.format(**{'region':region,'province':province})
-    configFile = open('eDamage_b0_{region}.ini'.format(**{'region':region}), 'w')
+    configFile = open('eDamage_SRMJobFiles_output/eDamage_b0_{region}.ini'.format(**{'region':region}), 'w')
     configFile.write(configContents)
     configFile.close()
     # configTemplate.close()
 
     configTemplate = open('eDamage_r2_template.ini', 'r').read()
     configContents = configTemplate.format(**{'region':region,'province':province})
-    configFile = open('eDamage_r2_{region}.ini'.format(**{'region':region}), 'w')
+    configFile = open('eDamage_SRMJobFiles_output/eDamage_r2_{region}.ini'.format(**{'region':region}), 'w')
     configFile.write(configContents)
     configFile.close()
     # configTemplate.close()
@@ -73,16 +79,18 @@ def write_e_damage_config(regionPair):
 def write_eb_risk_config(regionPair):
     province = regionPair[1]
     region = regionPair[0]
+    if not os.path.exists('ebRisk_SRMJobFiles_output'):
+        os.makedirs('ebRisk_SRMJobFiles_output')
     configTemplate = open('ebRisk_b0_template.ini', 'r').read()
     configContents = configTemplate.format(**{'region':region,'province':province})
-    configFile = open('ebRisk_b0_{region}.ini'.format(**{'region':region}), 'w')
+    configFile = open('ebRisk_SRMJobFiles_output/ebRisk_b0_{region}.ini'.format(**{'region':region}), 'w')
     configFile.write(configContents)
     configFile.close()
     # configTemplate.close()
 
     configTemplate = open('ebRisk_r2_template.ini', 'r').read()
     configContents = configTemplate.format(**{'region':region,'province':province})
-    configFile = open('ebRisk_r2_{region}.ini'.format(**{'region':region}), 'w')
+    configFile = open('ebRisk_SRMJobFiles_output/ebRisk_r2_{region}.ini'.format(**{'region':region}), 'w')
     configFile.write(configContents)
     configFile.close()
     # configTemplate.close()
@@ -90,9 +98,11 @@ def write_eb_risk_config(regionPair):
 
 
 def write_c_hazard_config(province):
+    if not os.path.exists('cHazard_SRMJobFiles_output'):
+        os.makedirs('cHazard_SRMJobFiles_output')
     configTemplate = open('cHazard_template.ini', 'r').read()
     configContents = configTemplate.format(**{'province':province})
-    configFile = open('cHazard_{province}.ini'.format(**{'province':province}), 'w')
+    configFile = open('cHazard_SRMJobFiles_output/cHazard_{province}.ini'.format(**{'province':province}), 'w')
     configFile.write(configContents)
     configFile.close()
     # configTemplate.close()
