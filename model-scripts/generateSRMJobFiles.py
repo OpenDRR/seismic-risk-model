88ba5c6d04c598ad767a9a400cb08c84011d6e44
Murray Journeay <53489116+mjourneay@users.noreply.github.com>
2020-09-27 16:10:57 -0700

    Automate job files updates (#17)
    * Put job files into neat directories
    
    * Write all config files
    
    * Formatting changes to templates for parsing
    
    * Automatically generate runfiles
    
    Co-authored-by: Drew Rotheram <drew.rotheram@gmail.coom>

--- model-scripts/generateSRMJobFiles.py
+++ model-scripts/generateSRMJobFiles.py
@@ -17,87 +17,140 @@ def main():
     
     provinceList = []
     for regionPair in listRegion:
-        #write_c_damage_config(regionPair)
-        #write_e_damage_config(regionPair)
-        #write_eb_risk_config(regionPair)
+        write_c_damage_config(regionPair)
+        write_c_damage_runFile(regionPair)
+        write_e_damage_config(regionPair)
+        write_e_damage_runFile(regionPair)
+        write_eb_risk_config(regionPair)
+        write_eb_risk_runFile(regionPair)
         provinceList.append(regionPair[1])
     
     provinceList = list(dict.fromkeys(provinceList))
 
     for province in provinceList:
         write_c_hazard_config(province)
+        write_c_hazard_runFile(province)
 
-    #write_c_damage_runFile()
-    #write_c_hazard_runFile()
-    #write_e_damage_runFile()
-    #write_eb_risk_runFile()
-
-
+    
 #Suport Functions
 def write_c_damage_config(regionPair):
     province = regionPair[1]
     region = regionPair[0]
+    if not os.path.exists('cDamage_SRMJobFiles_output'):
+        os.makedirs('cDamage_SRMJobFiles_output')
+
     configTemplate = open('cDamage_b0_template.ini', 'r').read()
     configContents = configTemplate.format(**{'region':region,'province':province})
-    configFile = open('cDamage_b0_{region}.ini'.format(**{'region':region}), 'w')
+    configFile = open('cDamage_SRMJobFiles_output/cDamage_b0_{region}.ini'.format(**{'region':region}), 'w')
     configFile.write(configContents)
     configFile.close()
     # configTemplate.close()
 
     configTemplate = open('cDamage_r2_template.ini', 'r').read()
     configContents = configTemplate.format(**{'region':region,'province':province})
-    configFile = open('cDamage_r2_{region}.ini'.format(**{'region':region}), 'w')
+    configFile = open('cDamage_SRMJobFiles_output/cDamage_r2_{region}.ini'.format(**{'region':region}), 'w')
     configFile.write(configContents)
     configFile.close()
     # configTemplate.close()
     return
 
+def write_c_damage_runFile(regionPair):
+    province = regionPair[1]
+    region = regionPair[0]
+    if not os.path.exists('SRMRunFiles_output'):
+        os.makedirs('SRMRunFiles_output')
+    runTemplate = open('run-cDamage_CA.sh', 'r').read()
+    runContents = runTemplate.format(**{'region':region,'province':province})
+    runFile = open('SRMRunFiles_output/run-cDamage_CA.sh', 'a')
+    runFile.write(runContents)
+    runFile.close()
+    return
+
 def write_e_damage_config(regionPair):
     province = regionPair[1]
     region = regionPair[0]
+    if not os.path.exists('eDamage_SRMJobFiles_output'):
+        os.makedirs('eDamage_SRMJobFiles_output')
+
     configTemplate = open('eDamage_b0_template.ini', 'r').read()
     configContents = configTemplate.format(**{'region':region,'province':province})
-    configFile = open('eDamage_b0_{region}.ini'.format(**{'region':region}), 'w')
+    configFile = open('eDamage_SRMJobFiles_output/eDamage_b0_{region}.ini'.format(**{'region':region}), 'w')
     configFile.write(configContents)
     configFile.close()
     # configTemplate.close()
 
     configTemplate = open('eDamage_r2_template.ini', 'r').read()
     configContents = configTemplate.format(**{'region':region,'province':province})
-    configFile = open('eDamage_r2_{region}.ini'.format(**{'region':region}), 'w')
+    configFile = open('eDamage_SRMJobFiles_output/eDamage_r2_{region}.ini'.format(**{'region':region}), 'w')
     configFile.write(configContents)
     configFile.close()
     # configTemplate.close()
     return
 
+def write_e_damage_runFile(regionPair):
+    province = regionPair[1]
+    region = regionPair[0]
+    # if not os.path.exists('SRMRunFiles_output'):
+    #     os.makedirs('SRMRunFiles_output')
+    runTemplate = open('run-eDamage_CA.sh', 'r').read()
+    runContents = runTemplate.format(**{'region':region,'province':province})
+    runFile = open('SRMRunFiles_output/run-eDamage_CA.sh', 'a')
+    runFile.write(runContents)
+    runFile.close()
+    return
+
 def write_eb_risk_config(regionPair):
     province = regionPair[1]
     region = regionPair[0]
+    if not os.path.exists('ebRisk_SRMJobFiles_output'):
+        os.makedirs('ebRisk_SRMJobFiles_output')
     configTemplate = open('ebRisk_b0_template.ini', 'r').read()
     configContents = configTemplate.format(**{'region':region,'province':province})
-    configFile = open('ebRisk_b0_{region}.ini'.format(**{'region':region}), 'w')
+    configFile = open('ebRisk_SRMJobFiles_output/ebRisk_b0_{region}.ini'.format(**{'region':region}), 'w')
     configFile.write(configContents)
     configFile.close()
     # configTemplate.close()
 
     configTemplate = open('ebRisk_r2_template.ini', 'r').read()
     configContents = configTemplate.format(**{'region':region,'province':province})
-    configFile = open('ebRisk_r2_{region}.ini'.format(**{'region':region}), 'w')
+    configFile = open('ebRisk_SRMJobFiles_output/ebRisk_r2_{region}.ini'.format(**{'region':region}), 'w')
     configFile.write(configContents)
     configFile.close()
     # configTemplate.close()
     return
 
+def write_eb_risk_runFile(regionPair):
+    province = regionPair[1]
+    region = regionPair[0]
+    # if not os.path.exists('SRMRunFiles_output'):
+    #     os.makedirs('SRMRunFiles_output')
+    runTemplate = open('run-ebRisk_CA.sh', 'r').read()
+    runContents = runTemplate.format(**{'region':region,'province':province})
+    runFile = open('SRMRunFiles_output/run-ebRisk_CA.sh', 'a')
+    runFile.write(runContents)
+    runFile.close()
+    return
 
 def write_c_hazard_config(province):
+    if not os.path.exists('cHazard_SRMJobFiles_output'):
+        os.makedirs('cHazard_SRMJobFiles_output')
     configTemplate = open('cHazard_template.ini', 'r').read()
     configContents = configTemplate.format(**{'province':province})
-    configFile = open('cHazard_{province}.ini'.format(**{'province':province}), 'w')
+    configFile = open('cHazard_SRMJobFiles_output/cHazard_{province}.ini'.format(**{'province':province}), 'w')
     configFile.write(configContents)
     configFile.close()
     # configTemplate.close()
     return
-    
+
+def write_c_hazard_runFile(province):
+    # if not os.path.exists('SRMRunFiles_output'):
+    #     os.makedirs('SRMRunFiles_output')
+    runTemplate = open('run-cHazard_CA.sh', 'r').read()
+    runContents = runTemplate.format(**{'province':province})
+    runFile = open('SRMRunFiles_output/run-cHazard_CA.sh', 'a')
+    runFile.write(runContents)
+    runFile.close()
+    return 
 
 def get_config_params():
     """
