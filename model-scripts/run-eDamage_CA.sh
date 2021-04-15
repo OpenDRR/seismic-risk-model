f69920b56b71a24b0e62b290d27bcca71bb92cee
Murray Journeay <murray.journeay@canada.ca>
2021-04-15 13:11:15 -0700

    updates to job.ini & basch run files

--- model-scripts/run-eDamage_CA.sh
+++ model-scripts/run-eDamage_CA.sh
@@ -10,12 +10,12 @@ oq export fullreport -1 -e rst -d output/{province}/
 mv output/{province}/report*.rst output/{province}/eD_{region}_report_b0.csv
 oq export realizations -1 -e csv -d output/{province}/
 mv output/{province}/realizations*.csv output/{province}/eD_{region}_rlz.csv
-oq export avg_damages-stats -1 -e csv -d output/{province}/
+oq export damages-stats -1 -e csv -d output/{province}/
 mv output/{province}/avg_damages-mean*.csv output/{province}/eD_{region}_damages-mean_b0.csv;
  # Retrofit Conditions
 oq engine --run input/eDamage_r1_{region}.ini &> output/{province}/eD_{region}_r1.log;
 oq export fullreport -1 -e rst -d output/{province}/
 mv output/{province}/report*.rst output/{province}/eD_{region}_report_r1.csv
-oq export avg_damages-stats -1 -e csv -d output/{province}/
+oq export damages-stats -1 -e csv -d output/{province}/
 mv output/{province}/avg_damages-mean*.csv output/{province}/eD_{region}_damages-mean_r2.csv;
 # =================================================================
