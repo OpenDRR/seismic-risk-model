45e86353903e295a336b7ebd3d78050c3a2a7494
Tiegan Hobbs <59665171+tieganh@users.noreply.github.com>
2021-05-19 19:13:10 -0700

    setting up Canada wide r1 for ebRisk test

--- ebRisk/run-ebRisk_Canada.sh
+++ ebRisk/run-ebRisk_Canada.sh
@@ -6,28 +6,28 @@ mkdir -p output/temp; rm -f output/temp/*
 # CANADA-WIDE
 # =================================================================
 # Baseline Conditions
-oq engine --run input/ebRisk_b0_Canada.ini > output/Canada/ebR_Canada_b0.log;
-oq export fullreport -1 -e rst -d output/temp/
-mv output/temp/report*.rst output/Canada/ebR_Canada_report_b0.rst
-oq export realizations -1 -e csv -d output/temp/
-mv output/temp/realizations*.csv output/Canada/ebR_Canada_rlz.csv
-oq export avg_losses-stats -1 -e csv -d output/temp/
-mv output/temp/avg_losses-mean*.csv output/Canada/ebR_Canada_avg_losses-stats_b0.csv;
-mv output/temp/avg_losses-quantile-0.05*.csv output/Canada/ebR_Canada_avg_losses-q05_b0.csv;
-mv output/temp/avg_losses-quantile-0.5*.csv output/Canada/ebR_Canada_avg_losses-q50_b0.csv;
-mv output/temp/avg_losses-quantile-0.95*.csv output/Canada/ebR_Canada_avg_losses-q95_b0.csv;
-oq export agg_curves-stats -1 -e csv -d output/temp/
-mv output/temp/agg_curves-stats*.csv output/Canada/ebR_Canada_agg_curves-stats_b0.csv;
-mv output/temp/agg_curves-quantile-0.05*.csv output/Canada/ebR_Canada_agg_curves-q05_b0.csv;
-mv output/temp/agg_curves-quantile-0.5*.csv output/Canada/ebR_Canada_agg_curves-q50_b0.csv;
-mv output/temp/agg_curves-quantile-0.95*.csv output/Canada/ebR_Canada_agg_curves-q95_b0.csv;
-oq export agg_losses-stats -1 -e csv -d output/temp/
-mv output/temp/agg_losses-mean*.csv output/ebR_Canada_agg_losses-stats_b0.csv;
-mv output/temp/agg_losses-quantile-0.05*.csv output/Canada/ebR_Canada_agg_losses-q05_b0.csv;
-mv output/temp/agg_losses-quantile-0.5*.csv output/Canada/ebR_Canada_agg_losses-q50_b0.csv;
-mv output/temp/agg_losses-quantile-0.95*.csv output/Canada/ebR_Canada_agg_losses-q95_b0.csv;
-oq export src_loss_table -1 -e csv -d output/temp/
-mv output/temp/src_loss_table_*.csv output/Canada/ebR_Canada_src_loss_table_b0.csv;
+#oq engine --run input/ebRisk_b0_Canada.ini > output/Canada/ebR_Canada_b0.log;
+#oq export fullreport -1 -e rst -d output/temp/
+#mv output/temp/report*.rst output/Canada/ebR_Canada_report_b0.rst
+#oq export realizations -1 -e csv -d output/temp/
+#mv output/temp/realizations*.csv output/Canada/ebR_Canada_rlz.csv
+#oq export avg_losses-stats -1 -e csv -d output/temp/
+#mv output/temp/avg_losses-mean*.csv output/Canada/ebR_Canada_avg_losses-stats_b0.csv;
+#mv output/temp/avg_losses-quantile-0.05*.csv output/Canada/ebR_Canada_avg_losses-q05_b0.csv;
+#mv output/temp/avg_losses-quantile-0.5*.csv output/Canada/ebR_Canada_avg_losses-q50_b0.csv;
+#mv output/temp/avg_losses-quantile-0.95*.csv output/Canada/ebR_Canada_avg_losses-q95_b0.csv;
+#oq export agg_curves-stats -1 -e csv -d output/temp/
+#mv output/temp/agg_curves-stats*.csv output/Canada/ebR_Canada_agg_curves-stats_b0.csv;
+#mv output/temp/agg_curves-quantile-0.05*.csv output/Canada/ebR_Canada_agg_curves-q05_b0.csv;
+#mv output/temp/agg_curves-quantile-0.5*.csv output/Canada/ebR_Canada_agg_curves-q50_b0.csv;
+#mv output/temp/agg_curves-quantile-0.95*.csv output/Canada/ebR_Canada_agg_curves-q95_b0.csv;
+#oq export agg_losses-stats -1 -e csv -d output/temp/
+#mv output/temp/agg_losses-mean*.csv output/ebR_Canada_agg_losses-stats_b0.csv;
+#mv output/temp/agg_losses-quantile-0.05*.csv output/Canada/ebR_Canada_agg_losses-q05_b0.csv;
+#mv output/temp/agg_losses-quantile-0.5*.csv output/Canada/ebR_Canada_agg_losses-q50_b0.csv;
+#mv output/temp/agg_losses-quantile-0.95*.csv output/Canada/ebR_Canada_agg_losses-q95_b0.csv;
+#oq export src_loss_table -1 -e csv -d output/temp/
+#mv output/temp/src_loss_table_*.csv output/Canada/ebR_Canada_src_loss_table_b0.csv;
 # Retrofit Conditions
 oq engine --run input/ebRisk_r1_Canada.ini > output/Canada/ebR_Canada_r1.log;
 oq export fullreport -1 -e rst -d output/temp/
