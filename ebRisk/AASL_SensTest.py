469e9c69008526fbcc012f641aef2d567d77462b
Tiegan Hobbs <59665171+tieganh@users.noreply.github.com>
2021-06-16 14:57:53 -0700

    setup full enum round 2 and edit python script for sensitivity test metrics

--- ebRisk/AASL_SensTest.py
+++ ebRisk/AASL_SensTest.py
@@ -4,3 +4,6 @@ import sys
 df = pd.read_csv(sys.argv[1], skiprows=1)
 totalLoss = df['structural'].sum()
 print("Total structural annual average loss is: ",totalLoss)
+
+# awk -F',' '($3=="structural") && ($1=="500") && ($4=="*total*") {print $2}' agg_curves-quantile-0.95_31.csv
+# awk -F',' '($3=="structural") && ($1=="500") && ($4=="*total*") {print $2}' agg_curves-mean_31.csv 
