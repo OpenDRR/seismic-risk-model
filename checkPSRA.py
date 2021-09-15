c5b138d010c7e978c488ae85d30594793c2438df
Tiegan Hobbs <59665171+tieganh@users.noreply.github.com>
2021-09-15 12:44:23 -0700

    start check PSRA, rerun eDam NB/NS with 50 years

---
+++ checkPSRA.py
@@ -0,0 +1,41 @@
+#!/bin/python
+
+#### Python script to check PSRA calculations
+
+import pandas as pd
+pd.options.mode.chained_assignment = None  # default='warn'
+import numpy as np
+
+##############################################################################
+### Load files and select SAUID of interest
+SAUID = 59007373
+num = 40 #index of the single building (asset) to display for bldg level calcs
+damME = pd.read_csv('eDamage/output/NS/eD_NS_damages-mean_b0.csv', skiprows=1)
+
+##############################################################################
+
+### Merge dataframes and isolate SAUID of interest
+
+##############################################################################
+### CALCULATE PSRA INDICATORS
+
+eDp_Collapse
+eD_None
+eDsd_None
+eDr_None
+eD_Slight
+eDsd_Slight
+eDr_Slight
+eD_Moderate
+eDsd_Moderate
+eDr_Moderate
+eD_Extensive
+eDsd_Extensive
+eDr_Extensive
+eD_Complete
+eDsd_Complete
+eDr_Complete
+eD_Collapse
+eDsd_Collapse
+eDr_Collapse
+
---
+++ eDamage/run_OQeDamage_NSNBTest.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# ==========================================================================
+# Script for running eDamage calculations in the National Canadian Risk Model
+# ==========================================================================
+usage() {
+echo "Script for running eDamage calculations in OpenQuake, using Canadian data. 
+You need to have created the ini files already. Written by TE Hobbs on 3 Aug 2021.
+
+
+USAGE: run_OQeDamage.sh 
+    to be run from the canada-srm/eDamage/ folder. Automatically runs b0 and r1.
+
+"
+}
+
+### SETUP AWS KILL 
+shut_down_ec2_instance() {
+    echo "Shutting down EC2 instance"
+    sudo shutdown
+    }
+
+trap "shut_down_ec2_instance" ERR
+
+### SETUP REGIONS
+declare -a regions=("NS" "NB") #eastern
+#need to go back to "ON3510" "ON3515-20" "ON3530A" "ON3540" "ON3550" "ON3560-70" "ON3580" "ON3590-95"
+# already did NB, NL, NS
+#declare -a regions=("AB4810-20", "AB4830", "AB4840-50", "AB4860", "AB4870-80", "BC5910", "BC5920A", "BC5920B", "BC5930", "BC5940-80", "MB4610-40", "MB4650-80", "NT", "NU", "SK4710-30", "SK4740-60", "YT") #western
+#declare -a regions=("ON3530B") #for testing only, omitted above
+
+
+### SETUP CALCULATIONS
+declare -a calcs=("b0" "r1")
+
+for region in "${regions[@]}"; do
+    calcnum="-2"
+    mkdir -p output/temp; rm -f output/temp/*
+    prov=`echo $region | cut -c1-2`
+    ### RUN RISK CALCS
+    oq engine --run input/eDamage_b0_${region}.ini input/eDamage_r1_${region}.ini &> output/${prov}/eD50_${region}_b0r1.log;
+    for calc in "${calcs[@]}"; do
+        ### EXPORTS
+        oq export fullreport $calcnum -e rst -d output/temp/
+        mv output/temp/report*.rst output/${prov}/eD50_${region}_report_${calc}.csv
+        if [[ ${calc} == "b0" ]]; then
+            oq export realizations $calcnum -e csv -d output/temp/
+            mv output/temp/realizations*.csv output/${prov}/eD50_${region}_rlz_b0.csv 
+        fi
+        oq export damages-stats $calcnum -e csv -d output/temp/
+        mv output/temp/avg_damages-mean*.csv output/${prov}/eD50_${region}_damages-mean_${calc}.csv;
+        mv output/temp/avg_damages-quantile-0.05*.csv output/${prov}/eD50_${region}_damages-q05_${calc}.csv;
+        mv output/temp/avg_damages-quantile-0.5*.csv output/${prov}/eD50_${region}_damages-q50_${calc}.csv;
+        mv output/temp/avg_damages-quantile-0.95*.csv output/${prov}/eD50_${region}_damages-q95_${calc}.csv;
+        calcnum=$((${calcnum}+1))
+    done
+done
+
+### AWS KILL
+shut_down_ec2_instance
